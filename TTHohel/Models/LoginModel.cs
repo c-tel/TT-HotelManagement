b8a7ec9909eb368b4d2321e769304b0171d69feb
dashatyshch <dasha.tyshenko98@gmail.com>
2019-03-15 21:01:23 +0200

    remove redundant usings & comments from LoginView/Model/ViewModel

--- TTHohel/Models/LoginModel.cs
+++ TTHohel/Models/LoginModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TTHohel.Manger;
+﻿using TTHohel.Manger;
 using TTHohel.Services;
 
 namespace TTHohel.Models
@@ -19,21 +14,6 @@ namespace TTHohel.Models
                 return true;
             }
             return false;
-            //if (!_storage.Users.ContainsKey(login))
-            //{
-            //    MessageBox.Show("Login or password is wrong");
-            //}
-            //else
-            //{
-            //    if (_storage.Users[login].Password != password)
-            //    {
-            //        MessageBox.Show("Login or password is wrong");
-            //    }
-            //    else
-            //    {
-            //        NavigationManager.Instance.Navigate(ModesEnum.Main);
-            //    }
-            //}
         }
     }
 }
