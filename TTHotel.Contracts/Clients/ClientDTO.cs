43914ce1af96a85a7ce8541fe2f5f82731e92ef4
c-tel <stas.telizhenko@gmail.com>
2019-03-28 21:39:30 +0200

    start ClientsController

---
+++ TTHotel.Contracts/Clients/ClientDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTHotel.Contracts.Clients
+{
+    public class ClientDTO
+    {
+        public string TelNum { get; set; }
+        public string Passport { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronym { get; set; }
+        public int Discount { get; set; }
+    }
+}
---
+++ TTHotelManagement/Controllers/ClientsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TTHotel.API.Services;
+using TTHotel.Contracts.Clients;
+
+namespace TTHotel.API.Controllers
+{
+    [ApiController]
+    [Route("api/clients")]
+    public class ClientsController: ControllerBase
+    {
+        private readonly IHotelService _hotelService;
+        public ClientsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet()]
+        public IEnumerable<ClientDTO> GetAll()
+        {
+            return _hotelService.GetClients();
+        }
+
+        [HttpPost()]
+        public ActionResult<IEnumerable<ClientDTO>> GetAll([FromBody] ClientDTO client)
+        {
+            _hotelService.CreateClient(client);
+            return NoContent();
+        }
+    }
+}
