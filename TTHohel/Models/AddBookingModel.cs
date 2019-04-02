a0193cb33c58c81f384c68d048261d73538047e4
dashatyshch <dasha.tyshenko98@gmail.com>
2019-04-02 12:16:38 +0300

    Changed AddBooking logic and view

--- TTHohel/Models/AddBookingModel.cs
+++ TTHohel/Models/AddBookingModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using TTHohel.Manager;
 
 namespace TTHohel.Models
 {
     class AddBookingModel
     {
+        public void GoToAddClient()
+        {
+            NavigationManager.Instance.Navigate(ModesEnum.Pay);
+        }
     }
 }
--- TTHohel/ViewModels/AddBookingViewModel.cs
+++ TTHohel/ViewModels/AddBookingViewModel.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using TTHohel.Models;
+using TTHohel.Tools;
 
 namespace TTHohel.ViewModels
 {
-    class AddBookingViewModel
+    class AddBookingViewModel : INotifyPropertyChanged
     {
+        #region Private Fields
+        private DateTime _dateFrom;
+        private DateTime _dateTo;
+
+        //private BookingDTO _bookingDTO;
+        private AddBookingModel Model { get; }
+
+        private ICommand _addClientCommand;
+        #endregion
+
+        public AddBookingViewModel()
+        {
+            Model = new AddBookingModel();
+        }
+
+        #region Commands
+        public ICommand AddClientCommand
+        {
+            get
+            {
+                if (_addClientCommand == null)
+                    _addClientCommand = new RelayCommand<object>(AddClientExecute, AddClientCanExecute);
+                return _addClientCommand;
+            }
+            set
+            {
+                _addClientCommand = value;
+                InvokePropertyChanged(nameof(AddClientCommand));
+            }
+        }
+
+        private bool AddClientCanExecute(object obj)
+        {
+            return true;
+        }
+
+        private void AddClientExecute(object obj)
+        {
+            Model.GoToAddClient();
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void InvokePropertyChanged(string propertyName)
+        {
+            var e = new PropertyChangedEventArgs(propertyName);
+            PropertyChanged?.Invoke(this, e);
+        }
+        #endregion
     }
 }
