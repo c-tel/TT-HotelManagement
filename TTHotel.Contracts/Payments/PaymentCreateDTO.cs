48a65333522f72643e99bf2a286c681d9c2fdf31
c-tel <stas.telizhenko@gmail.com>
2019-04-06 21:12:07 +0300

    added pay

--- TTHotel.Contracts/Payments/PaymentCreateDTO.cs
+++ TTHotel.Contracts/Payments/PaymentCreateDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +8,8 @@ namespace TTHotel.Contracts.Payments
 {
     public class PaymentCreateDTO
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public PaymentTypes Type { get; set; }
         public double Amount { get; set; }
-        public int BookingId { get; set; }
     }
 }
--- TTHotelManagement/DBEntities/Payment.cs
+++ TTHotelManagement/DBEntities/Payment.cs
@@ -9,7 +9,7 @@ namespace TTHotel.API.DBEntities
     public class Payment
     {
         public int Payment_num { get; set; }
-        public PaymentTypes Type { get; set; }
+        public PaymentTypes Payment_type { get; set; }
         public DateTime Payment_date { get; set; }
         public double Amount { get; set; }
         public int Book_num { get; set; }
