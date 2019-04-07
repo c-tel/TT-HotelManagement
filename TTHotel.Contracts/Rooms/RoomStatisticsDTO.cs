39ac6c5f8744a3dcbaf24b5aea78f7e615ea9978
c-tel <stas.telizhenko@gmail.com>
2019-04-07 22:25:08 +0300

    start room statistics

---
+++ TTHotel.Contracts/Rooms/RoomStatisticsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TTHohel.Contracts.Bookings;
+
+namespace TTHotel.Contracts.Rooms
+{
+    public class RoomStatisticsDTO
+    {
+        public int Num { get; set; }
+        public RoomType Type { get; set; }
+    }
+}
