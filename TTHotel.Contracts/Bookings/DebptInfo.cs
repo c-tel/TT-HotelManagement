39384e3784ddcb9b46658257293231156c827822
c-tel <stas.telizhenko@gmail.com>
2019-04-11 15:57:42 +0300

    add DebptInfo

---
+++ TTHotel.Contracts/Bookings/DebptInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTHotel.Contracts.Bookings
+{
+    public class DebptInfo
+    {
+        public int BookNum { get; set; }
+        public int RoomNum { get; set; }
+        public string ClientTelNum { get; set; }
+        public string ClientSurname { get; set; }
+        public string ClientName { get; set; }
+        public double Debpt { get; set; }
+    }
+}
