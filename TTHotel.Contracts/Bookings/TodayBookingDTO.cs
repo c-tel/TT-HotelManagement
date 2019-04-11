a269545f5aadf2288247225cf17ae024cfa4f10a
c-tel <stas.telizhenko@gmail.com>
2019-04-11 15:25:53 +0300

    add TodayBookingDTO

---
+++ TTHotel.Contracts/Bookings/TodayBookingDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TTHotel.Contracts.Bookings
+{
+    public class TodayBookingDTO
+    {
+        public int BookNum { get; set; }
+        public int RoomNum { get; set; }
+        public string ClientTelNum { get; set; }
+        public string ClientSurname { get; set; }
+        public string ClientName { get; set; }
+        public string BookComment { get; set; }
+    }
+}
